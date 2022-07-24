22aaed4533dd834f578fa6b1d382ee4ef826dae0
anushabanur <anushabanur@anushas-air>
2022-07-24 15:44:54 +0530

    first commit

---
+++ Day6/src/FOREx1.java
@@ -0,0 +1,11 @@
+
+public class FOREx1 {
+
+	public static void main(String[] args) {
+		for(int n = 0; n < 5; n = n+1){
+            System.out.println("Hello world");
+        }
+
+	}
+
+}
---
+++ Day6/src/FOREx2.java
@@ -0,0 +1,11 @@
+
+public class FOREx2 {
+
+	public static void main(String[] args) {
+		for(int x=0;x<11;x++){
+            System.out.print(x+" ");
+        }
+
+	}
+
+}
