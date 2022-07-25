331850161a35f32a91b7114400efaede25f5949e
anushabanur <anushabanur@anushas-air>
2022-07-25 13:15:32 +0530

    first commit

---
+++ Day8/src/Assignment.java
@@ -0,0 +1,14 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("27");
+		
+			}
+			System.out.println();
+		}
+	}
+
+}
